466aae6643ea035912a9b18b78fe3fbb9ca39fcb
han-gaeul <gmlakdcjs314@gmail.com>
2022-09-26 12:02:34 +0900

    0926 수업 정리

--- class_0926/class_0926/urls.py
+++ class_0926/class_0926/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from class0926App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('ping/', views.ping),
+    path('pong/', views.pong),
 ]
