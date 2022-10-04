0bae4e33aee7d02786f8e3cf94a18c9c6983e375
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-04 10:57:57 +0900

    게시판 목록 구현

--- 1004/class1004/class1004App/views.py
+++ 1004/class1004/class1004App/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import redirect, render
+from .models import Article
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'articles/index.html')
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles' : articles
+    }
+    return render(request, 'articles/index.html', context)
 
 def new(request):
     return render(request, 'articles/new.html')
 
 def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    Article.objects.create(title=title, content=content)
+
     return redirect('articles:index')
