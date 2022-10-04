06683a3838a5fcdea715b1bbe0d713b8f3c43d1e
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-04 12:08:36 +0900

    ModelForm

--- 1004/class1004/class1004App/views.py
+++ 1004/class1004/class1004App/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 
@@ -10,13 +11,27 @@ def index(request):
     }
     return render(request, 'articles/index.html', context)
 
-def new(request):
-    return render(request, 'articles/new.html')
+# def new(request):
+#     article_form = ArticleForm()
+#     context = {
+#         'article_form' : article_form
+#     }
+#     return render(request, 'articles/new.html', context)
 
 def create(request):
-    title = request.POST.get('title')
-    content = request.POST.get('content')
+    # title = request.POST.get('title')
+    # content = request.POST.get('content')
 
-    Article.objects.create(title=title, content=content)
+    # Article.objects.create(title=title, content=content)
 
-    return redirect('articles:index')
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    else:
+        article_form = ArticleForm()
+    context = {
+        'article_form' : article_form
+    }
+    return render(request, 'articles/new.html', context=context)
