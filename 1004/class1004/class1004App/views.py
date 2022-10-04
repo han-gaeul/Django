1656d3eca43e38d07b5b0819ed6cc3689f59fa45
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-04 12:26:43 +0900

    update (GET)

--- 1004/class1004/class1004App/views.py
+++ 1004/class1004/class1004App/views.py
@@ -42,3 +42,11 @@ def detail(request, pk):
         'article' : article
     }
     return render(request, 'articles/detail.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article_form = ArticleForm(instance=article)
+    context = {
+        'article_form' : article_form
+    }
+    return render(request, 'articles/update.html', context)
