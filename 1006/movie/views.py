9fab82410d9792a6623d9ab93e6aac9bcb85792a
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-06 22:33:54 +0900

    html 코드 작성

--- 1006/movie/views.py
+++ 1006/movie/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import redirect, render
 from .models import Movie
+from .form import MovieForm
 
 # Create your views here.
 
 # 영화 데이터 목록 조회
 def movies(request):
-    return render(request, 'movies/movies.html')
+    movies = Movie.objects.all()
+    context = {
+        'movies' : movies
+    }
+    return render(request, 'movies/movies.html', context)
 
 # 영화 데이터 정보 조회
 def detail(request, pk):
-    return render(request, 'movies/detail.html')
+    movies = Movie.objects.get(pk=pk)
+    context = {
+        'movies' : movies
+    }
+    return render(request, 'movies/detail.html', context)
 
 # 영화 데이터 생성
 def create(request):
-    return render(request, 'movies/create.html')
+    if request.method == 'POST':
+        movies_form = MovieForm(request.POST)
+        
+        if movies_form.is_valid():
+            movies_form.save()
+            return redirect('movie:movies')
+    
+    else:
+        movies_form = MovieForm()
+    
+    context = {
+        'movies_form' : movies_form
+    }
+    return render(request, 'movies/create.html', context=context)
 
 # 영화 데이터 수정
 def update(request, pk):
+    movie = Movie.objects.get()
     return render(request, 'movies/update.html')
 
 # 영화 데이터 삭제
