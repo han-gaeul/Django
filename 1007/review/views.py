fa42398c30a45a4da6c03b1c3b2c6eed77a1f53f
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-09 17:24:45 +0900

    index/base.html

--- 1007/review/views.py
+++ 1007/review/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
+
+def index(request):
+    reviews = Review.objects.order_by('-pk')
+    context = {
+        'reviews' : reviews
+    }
+    return render(request, 'review/index.html', context)
+
