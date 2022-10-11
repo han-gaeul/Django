8dcca0a9b97766c144fc31531953fcd25cebe37a
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-11 22:07:54 +0900

    form fields 추가

--- 1011/accounts/forms.py
+++ 1011/accounts/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth import get_user_model
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('username', )
+        fields = ('username', 'email', 'password1', 'password2')
