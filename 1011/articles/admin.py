707020f1253ff78c71b1a5b03bbefaa577479d1a
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-11 16:30:35 +0900

    admin/static

--- 1011/articles/admin.py
+++ 1011/articles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ['titls']
+
+admin.site.register(Article, ArticleAdmin)
