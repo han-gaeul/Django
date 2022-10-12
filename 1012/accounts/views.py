a70f481fd1d1550f8e3bddbd3461e4c25e5a4d9b
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-12 17:12:19 +0900

    login edit

--- 1012/accounts/views.py
+++ 1012/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -48,7 +49,19 @@ def detail(request, pk):
     return render(request, 'accounts/detail.html', context)
 
 def sign(request):
-    form = AuthenticationForm()
+    if request.method == 'POST':
+        # AuthenticationForm은 ModelForm이 아님
+            form = AuthenticationForm(request, data=request.POST)
+
+            if form.is_valid():
+                # 세션에 저장
+                # login 함수는 request, user 객체를 인자로 받음
+                # user 객체는 form에서 인증된 우저 정보를 받을 수 있음
+                auth_login(request, form.get_user())
+                return redirect('articles:index')
+    
+    else:
+        form = AuthenticationForm()
     context = {
         'form' : form
     }
