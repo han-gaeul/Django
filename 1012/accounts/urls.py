89f31a1f15ae8e862e365e6d7fc74723169a0015
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-13 09:03:55 +0900

    logout/signup

--- 1012/accounts/urls.py
+++ 1012/accounts/urls.py
@@ -4,18 +4,14 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    # 메인 페이지
-    # http://127.0.0.1:8000/
-    path('', views.index, name='index'),
     # 회원 가입 페이지
     # http://127.0.0.1:8000/accounts/signup/
     path('signup/', views.signup, name='signup'),
-    # 회원 목록 페이지
-    # http://127.0.0.1:8000/accounts/
-    path('accounts/', views.accounts, name='accounts'),
     # 회원 조회 페이지 (프로필)
     # http://127.0.0.1:8000/accounts/<user_pk>/
     path('detail/<int:pk>', views.detail, name='detail'),
     # 로그인
     path('login/', views.login, name='login'),
+    # 로그아웃
+    path('logout/', views.logout, name='logout'),
 ]
--- 1012/accounts/views.py
+++ 1012/accounts/views.py
@@ -1,41 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserCreationForm
 
 # Create your views here.
-
-def index(request):
-    return render(request, 'accounts/index.html')
-
-from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
-from .forms import CustomUserCreationForm
-from django.contrib.auth import get_user_model
-
-# Create your views here.
-def index(request):
-    return render(request, 'accounts/index.html')
-
-def accounts(request):
-    accounts = get_user_model().objects.order_by('pk')
-    context = {
-        'accounts' : accounts
-    }
-    return render(request, 'accounts/accounts.html', context)
-
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
-
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request, user)
             return redirect('accounts:index')
-
     else:
         form = CustomUserCreationForm()
-
     context = {
         'form' : form
     }
@@ -48,21 +27,23 @@ def detail(request, pk):
     }
     return render(request, 'accounts/detail.html', context)
 
-def sign(request):
+def login(request):
     if request.method == 'POST':
         # AuthenticationForm은 ModelForm이 아님
             form = AuthenticationForm(request, data=request.POST)
-
             if form.is_valid():
                 # 세션에 저장
                 # login 함수는 request, user 객체를 인자로 받음
                 # user 객체는 form에서 인증된 우저 정보를 받을 수 있음
                 auth_login(request, form.get_user())
                 return redirect('articles:index')
-    
     else:
         form = AuthenticationForm()
     context = {
         'form' : form
     }
     return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
