44e743e46d16138756d6cd6eb9d4279b4ee2cdcb
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-14 00:03:16 +0900

    articles

--- 1013/PJT/settings.py
+++ 1013/PJT/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'articles',
     'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ 1013/articles/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+import articles
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles' : articles
+    }
+    return render(request, 'articles/index.html', context)
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    else:
+        article_from = ArticleForm()
+    context = {
+        'article_from' : article_form
+    }
+    return render(request, 'articles/form.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article
+    }
+    return render(request, 'articles/detail.html', context)
+
+@login_required
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        article_form = ArticleForm(instance=article)
+    context = {
+        'article_form' : article_form
+    }
+    return render(request, 'articles/form.html', context)
