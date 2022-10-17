52251dada6ebaca9cb2b5885e3c22cff61ba2702
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-18 01:41:04 +0900

    articles - views.py

--- 1017/articles/views.py
+++ 1017/articles/views.py
@@ -1,22 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 # 목록 조회
 def index(request):
-    return render(request, 'articles/index.html')
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles' : articles
+    }
+    return render(request, 'articles/index.html', context)
 
 # 정보 조회
 def detail(request, pk):
-    return render(request, 'articles/detail.html')
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article
+    }
+    return render(request, 'articles/detail.html', context)
 
 # 생성
 def create(request):
-    return render(request, 'articles/new.html')
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, request.FILES)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    else:
+        article_form = ArticleForm()
+    context = {
+        'article_form' : article_form
+    }
+    return render(request, 'articles/new.html', context=context)
 
 # 수정
 def update(request, pk):
-    return render(request, 'articles/update.html')
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, request.FILES, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        article_form = ArticleForm(instance=article)
+    context = {
+        'article_form' : article_form
+    }
+    return render(request, 'articles/update.html', context)
 
 # 삭제
-def delete(request):
-    return render()
+def delete(request, pk):
+    Article.objects.get(pk=pk).delete()
+    return redirect('articles:index')
