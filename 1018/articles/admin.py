b59f82a5738d6f93ea689f2070a754a4a173186a
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-18 20:27:56 +0900

    articles - models.py

--- 1018/articles/admin.py
+++ 1018/articles/admin.py
@@ -3,10 +3,10 @@ from .models import Article, Comment
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title')
+    list_display  = ('title', )
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('content')
+    list_display  = ('content', 'created_at', 'article')
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment, CommentAdmin)
