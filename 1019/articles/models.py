444864e9cc63190c3380db69e408a668bbc78458
han-gaeul <gmlakdcjs314@gmail.com>
2022-10-19 10:02:38 +0900

    PJT/app

---
+++ 1019/articles/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.forms import ImageField
+from imagekit.models import ProcessedImageField, ImageSpecField
+from imagekit.processors import Thumbnail, ResizeToFill
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = ProcessedImageField(
+        upload_to = 'images/', blank=True,
+        processors=[Thumbnail(500, 500)],
+        format='JPEG',
+        options={'quality': 100}
+    )
+    thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(120, 120)],
+        format='JPEG'
+    )
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
